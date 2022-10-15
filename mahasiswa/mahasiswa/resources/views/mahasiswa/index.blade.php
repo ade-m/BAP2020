cd5a9e20c5e0823c8d6fe4781991ee23f5216cbf
ade-m <66313509+ade-m@users.noreply.github.com>
2022-10-15 14:56:07 +0700

    pt 6

--- mahasiswa/mahasiswa/resources/views/mahasiswa/index.blade.php
+++ mahasiswa/mahasiswa/resources/views/mahasiswa/index.blade.php
@@ -1,36 +1,46 @@
 <?php
     use App\Models\mahasiswa;
+    use App\Models\vjlhmhskrs;
 ?>
-<html>
-    <head>
-        <title>Index Mahasiswa</title>
-    </head>
-    <body>
-        <h1>Index Mahasiswa</h1>
-        <style>
-            table tr{
-                padding: 10px;
-            }
-        </style>
-        <table style="padding: 10px;">
-            <th>
-                <tr>Student ID</tr>
-                <tr>Nama</tr>
-                <tr>Jurusan</tr>
-                <tr>Tahun</tr>
-            </th>
-            <?php
-            $mahasiswas = mahasiswa::get();
+@extends('layouts.app')
+
+@section('title','Mahasiswa')
+
+@section('content')
+    <h1>Index Mahasiswa</h1>
+    <style>
+        table tr{
+            padding: 10px;
+        }
+    </style>
+    <table style="padding: 10px;">
+        <tr>
+            <th>Student ID</th>
+            <th>Nama</th>
+            <th>Jumlah KRS</th>
+        </tr>
+        <?php
+        // $mahasiswas = mahasiswa::get();
+
+        // foreach ($mahasiswas as $mahasiswa) {
+        //     echo "<tr>";
+        //     echo "<td>".$mahasiswa->studentId."</td>";
+        //     echo "<td>".$mahasiswa->nama."</td>";
+        //     echo "<td>".$mahasiswa->jurusan."</td>";
+        //     echo "<td>".$mahasiswa->tahunMasuk."</td>";
+        //     echo "</tr>";
+        // }
+
+        $rows = \DB::select('select studentid, nama, jlhkrs from vjlhkrsmhs');
  
-            foreach ($mahasiswas as $mahasiswa) {
-                echo "<tr>";
-                echo "<td>".$mahasiswa->studentId."</td>";
-                echo "<td>".$mahasiswa->nama."</td>";
-                echo "<td>".$mahasiswa->jurusan."</td>";
-                echo "<td>".$mahasiswa->tahunMasuk."</td>";
-                echo "</tr>";
-            }
-            ?>
-        </table>
-    </body>
-</html>
+
+        foreach ($rows as $row) {
+            echo "<tr>";
+            echo "<td>".$row->studentid."</td>";
+            echo "<td>".$row->nama."</td>";
+            echo "<td>".$row->jlhkrs."</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+@endsection
